89484e9976c93e114240e17b1bcfa4bbfd2ecf77
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-03-23 16:44:08 +0300

    user model

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "main",
+    "main.user",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "main_user.User"
