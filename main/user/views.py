75698f658ae1efa2871d468185baaf5e4c59ec4c
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-03-23 17:17:32 +0300

    user serializer, viewset, router

--- main/user/views.py
+++ main/user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
-# Create your views here.
+from main.user.serializers import UserSerializer
+from main.user.models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    http_method_names = ("patch", "get")
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return User.objects.all()
+        return User.objects.exclude(is_superuser=True)
+
+    def get_object(self):
+        obj = User.objects.get_object_by_public_id(self.kwargs["pk"])
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
