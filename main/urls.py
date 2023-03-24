91650ace322497656d9c0c4925ae075bc259f079
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-03-24 16:53:05 +0300

    update and delete comment

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-from rest_framework import routers
+
+# from rest_framework import routers
+from rest_framework_nested import routers
+
 from main.user.views import UserViewSet
 from main.post.views import PostViewSet
+from main.comment.views import CommentViewSet
 
 from main.auth.views import RegisterViewSet, LoginViewSet, RefreshViewSet
 
@@ -15,4 +19,10 @@ router.register(r"user", UserViewSet, basename="user")
 
 router.register(r"post", PostViewSet, basename="post")
 
-urlpatterns = [path("", include(router.urls))]
+posts_router = routers.NestedSimpleRouter(router, r"post", lookup="post")
+posts_router.register(r"comment", CommentViewSet, basename="post-comment")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(posts_router.urls)),
+]
