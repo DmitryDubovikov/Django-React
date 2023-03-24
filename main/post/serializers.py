ab6ef8252024d4e113fce6673c1fda929f5fb1dc
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-03-24 12:21:23 +0300

    PostModel, PostSerializer

---
+++ main/post/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from main.abstract.serializers import AbstractSerializer
+from main.post.models import Post
+from main.user.models import User
+
+
+class PostSerializer(AbstractSerializer):
+    author = serializers.SlugRelatedField(
+        queryset=User.objects.all(), slug_field="public_id"
+    )
+
+    def validate_author(self, value):
+        if self.context["request"].user != value:
+            raise ValidationError("You can't create a post for another user.")
+        return value
+
+    class Meta:
+        model = Post
+        # List of all the fields that can be included in a
+        # request or a response
+        fields = ["id", "author", "body", "edited", "created", "updated"]
+        read_only_fields = ["edited"]
--- project/settings.py
+++ project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "main",
     "main.user",
     "main.auth",
+    "main.post",
 ]
 
 MIDDLEWARE = [
